17cbd8766bf319f8ca24d5f223a6f0061f80fdfa
Priyanka100995 <50015880+Priyanka100995@users.noreply.github.com>
2021-08-23 08:53:17 +0200

    Add files via upload

---
+++ 3_ModularCode_Conv4/Conv4Model.py
@@ -0,0 +1,109 @@
+""" Conv2Model for initialization of VGG Convoultional Neural Network """
+
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.models import Sequential
+from tensorflow_model_optimization.python.core.sparsity.keras.prune import prune_low_magnitude
+from tensorflow_model_optimization.python.core.sparsity.keras.pruning_callbacks import UpdatePruningStep
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+import yaml
+import Dataset
+import Prune
+
+# Load yaml file
+with open("C:/Users/Admin/PycharmProjects/ModularCode_Conv4/Experiment_Conv4.yaml", "r") as stream:
+    config = yaml.safe_load(stream)
+
+tf.random.set_seed(config["seed"])
+
+if config["enable_gpu"]:
+    physical_devices= tf.config.experimental.list_physical_devices('GPU')
+    print("Num GPUs Available: ", len(physical_devices))
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+
+class pruned_nn():
+
+    def pruned_nn(pruning_params_conv, pruning_params_fc, pruning_params_op):
+        lr = config["learning_rate"]
+        # Initializer
+        if config["kernel_init"] == "GlorotUniform":
+            init = tf.keras.initializers.GlorotUniform(seed=config["seed"])
+        elif config["kernel_init"] == "GlorotNormal":
+            init = tf.keras.initializers.GlorotNormal(seed=config["seed"])
+
+        pruned_model = Sequential()
+        pruned_model.add(prune_low_magnitude(
+            Conv2D(filters=64, kernel_size=(3, 3),activation=config["activation_function"], kernel_initializer=init,
+                strides=(1, 1), padding='same',input_shape=(32, 32, 3)),
+            **pruning_params_conv))
+
+        pruned_model.add(prune_low_magnitude(
+            Conv2D(filters=64, kernel_size=(3, 3), activation=config["activation_function"], kernel_initializer=init,
+                   strides=(1, 1), padding='same', input_shape=(32, 32, 3)),
+            **pruning_params_conv))
+
+        pruned_model.add(prune_low_magnitude(
+            MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+            **pruning_params_conv))
+
+        pruned_model.add(prune_low_magnitude(
+            Conv2D(
+                filters=128, kernel_size=(3, 3), activation=config["activation_function"], kernel_initializer=init,
+                strides=(1, 1), padding='same'),
+            **pruning_params_conv))
+
+        pruned_model.add(prune_low_magnitude(
+            Conv2D(
+                filters=128, kernel_size=(3, 3), activation=config["activation_function"], kernel_initializer=init,
+                strides=(1, 1), padding='same'),
+            **pruning_params_conv))
+
+        pruned_model.add(prune_low_magnitude(
+            MaxPooling2D(pool_size=(2, 2),strides=(2, 2)),
+            **pruning_params_conv))
+
+        pruned_model.add(Flatten())
+
+        pruned_model.add(prune_low_magnitude(
+            Dense(units=config["neuron_units"]["Dense1"], activation=config["activation_function"], kernel_initializer=init),
+            **pruning_params_fc))
+        # pruned_model.add(l.Dropout(0.2))
+
+        pruned_model.add(prune_low_magnitude(
+            Dense(units=config["neuron_units"]["Dense2"], activation=config["activation_function"], kernel_initializer=init),
+            **pruning_params_fc))
+        # pruned_model.add(l.Dropout(0.1))
+        pruned_model.add(prune_low_magnitude(
+            Dense(units=config["neuron_units"]["Output"], activation=config["output_activation"]),
+            **pruning_params_op))
+
+        # Compile pruned CNN-
+        pruned_model.compile(
+            loss=tf.keras.losses.categorical_crossentropy,
+            # optimizer='adam',
+            optimizer=tf.keras.optimizers.Adam(lr),
+            metrics=['accuracy'])
+
+        return pruned_model
+
+    # Add a pruning step callback to peg the pruning step to the optimizer's
+    # step.
+    callback = [
+        UpdatePruningStep(),
+        tf.keras.callbacks.EarlyStopping(
+            monitor=config["monitor"], patience=config["patience"],
+            min_delta=config["min_delta"]
+        )
+    ]
+
+    pruning_params_unpruned = {
+        'pruning_schedule': Prune.ConstantSparsity(
+            target_sparsity=0.0, begin_step=0,
+            end_step=Dataset.Data.end_step, frequency=100
+        )
+    }
+
+    # Initialize a CNN model-
+    orig_model = pruned_nn(pruning_params_unpruned, pruning_params_unpruned, pruning_params_unpruned)
+    orig_model.summary()
---
+++ 3_ModularCode_Conv4/PruningPercent.py
@@ -0,0 +1,168 @@
+"""PruningPercent to calculate percentages of weights to be pruned in every pruning round.
+   Extracted from yaml file:
+   -Percent to be pruned (P = 0.8, inorderto calculate 20% from every pruning round),
+   -maximum pruning performed is till 0.2% (Extracted from yaml file) of all parameters,
+   -maximum number of rounds for which pruning is to be carried out (until 0.2% of total weights)
+   #Option between P%^1/n and P% from every pruning round
+
+ """
+
+import tensorflow as tf
+import numpy as np
+import yaml
+
+# Load yaml file
+with open("C:/Users/Admin/PycharmProjects/ModularCode_Conv4/Experiment_Conv4.yaml", "r") as stream:
+    config = yaml.safe_load(stream)
+
+tf.random.set_seed(config["seed"])
+
+if config["enable_gpu"]:
+    physical_devices= tf.config.experimental.list_physical_devices('GPU')
+    print("Num GPUs Available: ", len(physical_devices))
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+
+#Edit values after summary is printed in the output
+# number of convolutional parameters-
+conv1 = config["conv1"]
+conv2 = config["conv2"]
+conv3 = config["conv3"]
+conv4 = config["conv4"]
+#number of fully-connected dense parameters-
+dense1 = config["dense1"]
+dense2 = config["dense2"]
+op_layer = config["op_layer"]
+
+# total number of parameters-
+total_params = conv1 + conv2 + conv3 + conv4 + dense1 + dense2 + op_layer
+
+print("\nTotal number of trainable parameters = {0}\n".format(total_params))
+
+# maximum pruning performed is till 0.2% of all parameters-
+max_pruned_params = config["Max_Pruning"]
+loc_tot_params = total_params
+loc_conv1 = conv1
+loc_conv2 = conv2
+loc_conv3 = conv3
+loc_conv4 = conv4
+loc_dense1 = dense1
+loc_dense2 = dense2
+loc_op_layer = op_layer
+
+# variable to count number of pruning rounds-
+n = 0
+
+# Lists to hold percentage of weights pruned in each round for all layers in CNN-
+conv1_pruning = []
+conv2_pruning = []
+conv3_pruning = []
+conv4_pruning = []
+dense1_pruning = []
+dense2_pruning = []
+op_layer_pruning = []
+
+
+num_pruning_rounds = config["num_of_pruning_rounds"]
+
+
+
+Pruning = config["Pruning_Option1"]
+# Percent for pruning
+if Pruning == '20% from every layer':
+
+        while loc_tot_params >= max_pruned_params:
+                loc_conv1 *= config["conv_pruning"]  # 10% weights are pruned
+                loc_conv2 *= config["conv_pruning"]   # 10% weights are pruned
+                loc_conv3 *= config["conv_pruning"]  # 10% weights are pruned
+                loc_conv4 *= config["conv_pruning"]  # 10% weights are pruned
+                loc_dense1 *= config["dense_pruning"]  # 20% weights are pruned
+                loc_dense2 *= config["dense_pruning"]   # 20% weights are pruned
+                loc_op_layer *= config["output_pruning"]   # 10% weights are pruned
+
+                conv1_pruning.append(((conv1 - loc_conv1) / conv1) * 100)
+                conv2_pruning.append(((conv2 - loc_conv2) / conv2) * 100)
+                conv3_pruning.append(((conv3 - loc_conv3) / conv3) * 100)
+                conv4_pruning.append(((conv4 - loc_conv4) / conv4) * 100)
+                dense1_pruning.append(((dense1 - loc_dense1) / dense1) * 100)
+                dense2_pruning.append(((dense2 - loc_dense2) / dense2) * 100)
+                op_layer_pruning.append(((op_layer - loc_op_layer) / op_layer) * 100)
+
+                loc_tot_params = loc_conv1 + loc_conv2 + loc_conv3 + loc_conv4 + loc_dense1 + loc_dense2 + loc_op_layer
+
+                n += 1
+
+                print("Conv1 = {0:.3f}, Conv2 = {1:.3f}, Conv3 = {2:.3f}, Conv4 = {3:.3f},  Dense1 = {4:.3f}, Dense2 = {5:.3f} & O/p layer = {6:.3f}".format(
+                        loc_conv1, loc_conv2, loc_conv3, loc_conv4, loc_dense1, loc_dense2, loc_op_layer))
+                print("Total number of parameters = {0:.3f}\n".format(loc_tot_params))
+                if n == num_pruning_rounds:
+                        break
+
+
+else:
+        for i in range(1, num_pruning_rounds + 1):
+                prune_percent = (2 ** (1 / i))
+
+                print("i", i)
+                print("Prune Percentage", prune_percent)
+                loc_conv1 = loc_conv1 - (loc_conv1 * prune_percent)
+                loc_conv2 = loc_conv2 - (loc_conv2 * prune_percent)
+                loc_conv3 = loc_conv3 - (loc_conv3 * prune_percent)
+                loc_conv4 = loc_conv4 - (loc_conv4 * prune_percent)
+                loc_dense1 = loc_dense1 - (loc_dense1 * prune_percent)  # 20% weights are pruned
+                loc_dense2 = loc_dense2 - (loc_dense2 * prune_percent)  # 20% weights are pruned
+                loc_op_layer = loc_op_layer - (loc_op_layer * prune_percent)  # 20% weights are pruned
+
+                conv1_pruning.append(prune_percent)
+                conv2_pruning.append(prune_percent)
+                conv3_pruning.append(prune_percent)
+                conv4_pruning.append(prune_percent)
+                dense1_pruning.append(prune_percent)
+                dense2_pruning.append(prune_percent)
+                op_layer_pruning.append(prune_percent)
+
+                loc_tot_params = loc_conv1 + loc_conv2 + loc_conv3 + loc_conv4 + loc_dense1 + loc_dense2 + loc_op_layer
+                n += 1
+
+                print(
+                        "Conv1 = {0:.3f}, Conv2 = {1:.3f}, Conv3 = {2:.3f}, Conv4 = {3:.3f},  Dense1 = {4:.3f}, Dense2 = {5:.3f} & O/p layer = {6:.3f}".format(
+                                loc_conv1, loc_conv2, loc_conv3, loc_conv4, loc_dense1, loc_dense2, loc_op_layer))
+                print("Total number of parameters = {0:.3f}\n".format(loc_tot_params))
+                loc_tot_params = loc_conv1+ loc_conv2+ loc_conv3+ loc_conv4+ loc_dense1 + loc_dense2 + loc_op_layer
+
+print("\nnumber of pruning rounds = {0}\n\n".format(num_pruning_rounds))
+
+
+# Convert from list to np.array-
+conv1_pruning = np.array(conv1_pruning)
+conv2_pruning = np.array(conv2_pruning)
+conv3_pruning = np.array(conv3_pruning)
+conv4_pruning = np.array(conv4_pruning)
+dense1_pruning = np.array(dense1_pruning)
+dense2_pruning = np.array(dense2_pruning)
+op_layer_pruning = np.array(op_layer_pruning)
+
+dense1_pruning = np.insert(dense1_pruning,0,0)
+conv1_pruning = np.insert(conv1_pruning,0,0)
+dense2_pruning = np.insert(dense2_pruning,0,0)
+conv2_pruning = np.insert(conv2_pruning,0,0)
+op_layer_pruning = np.insert(op_layer_pruning,0,0)
+# Round off numpy arrays to 5 decimal digits-
+conv1_pruning = np.round(conv1_pruning, decimals=5)
+conv2_pruning = np.round(conv2_pruning, decimals=5)
+conv3_pruning = np.round(conv3_pruning, decimals=5)
+conv4_pruning = np.round(conv4_pruning, decimals=5)
+dense1_pruning = np.round(dense1_pruning, decimals=5)
+dense2_pruning = np.round(dense2_pruning, decimals=5)
+op_layer_pruning = np.round(op_layer_pruning, decimals=5)
+
+conv1_pruning = conv1_pruning / 100
+conv2_pruning = conv2_pruning / 100
+conv3_pruning = conv3_pruning / 100
+conv4_pruning = conv4_pruning / 100
+dense1_pruning = dense1_pruning / 100
+dense2_pruning = dense2_pruning / 100
+op_layer_pruning = op_layer_pruning / 100
+
+#print(conv1_pruning)
+print("\nNumber of pruning rounds for LeNet NN = {0}\n".format(num_pruning_rounds))
